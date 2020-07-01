409147d2003fe1f5ec31ab34246842f48025b59f
Martin Cevik <mart706g@edu.ucl.dk>
2020-07-01 09:58:12 +0200

    ny commit

--- JS/flexslider.js
+++ JS/flexslider.js
@@ -1,13 +1,13 @@
-$(window).load(function() {
-    $('.front-slider').flexslider({
-        animation: "slide",
-        controlNav: false,
-        directionNav: false
-    });
+$(window).load(function () {
+  $(".front-slider").flexslider({
+    animation: "slide",
+    controlNav: false,
+    directionNav: false,
+  });
 
-    $('.story-slider').flexslider({
-        animation: "slide",
-        controlNav: true,
-        directionNav: true
-    });
+  $(".story-slider").flexslider({
+    animation: "slide",
+    controlNav: true,
+    directionNav: true,
+  });
 });
